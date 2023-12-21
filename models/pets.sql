03b09992394efc5efb682ab4756cff10d983cfc2
[red.git] <[geetikamanne44@gmail.com]>
2023-12-20 23:18:35 -0600

    push

---
+++ models/pets.sql
@@ -0,0 +1,13 @@
+
+select
+pets_col:Address.City::string as City,
+pets_col:Address."House Number"::string as House_No,
+pets_col:Address.State::string as State,
+pets_col:DOB::date as date1,
+pets_col:Gender::string as gender,
+pets_col:Name::string as Name,
+f1.value::string as Pets,
+pets_col:Phone.Mobile::number as Mobile,
+pets_col:Phone.Work::number as Work
+from pets1dbt,
+table(flatten(pets_col:Pets)) f1
